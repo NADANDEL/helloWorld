91fe576b4aa355b7c94a83c59ac3362e6e7395b3
NADANDEL <nadyaelifyildirim@gmail.com>
2023-10-22 20:45:46 +0200

    first commit

---
+++ src/DataTypesAndVariables.java
@@ -0,0 +1,16 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+    public class DataTypesAndVariables {
+    public static void main(String[] args) {
+
+    int age = 25;
+    char gender = 'F';
+    boolean retiredOrNot = false;
+
+
+    int birthyear = 1994;
+    int date = 2023;
+
+    System.out.println("Your age:" + (date - birthyear));
+    }
+}
